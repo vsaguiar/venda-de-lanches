dfd705eded11040a4f4fd9fc897b2a5e777d18ed
Vinicius Silva <vsaguiar@outlook.com>
2023-06-01 19:02:01 -0300

    Aplicando Data Annotations e gerando o banco de dados

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,15 +1,47 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanhces")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+        
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")] // Campo obrigatório
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informado")] // Campo obrigatório
+        [Display(Name = "Descrição do Lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "Descrição deve ter no mínimo {2} e no máximo {1}")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informado")] // Campo obrigatório
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "Descrição deve ter no mínimo {2} e no máximo {1}")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do lanche")] // Campo obrigatório
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
         public int CategoriaId { get; set; } // Chave estrangeira
         public virtual Categoria Categoria { get; set; }
