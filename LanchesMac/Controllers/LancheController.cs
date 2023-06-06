bbf996ac7bf4e6793a5415d77e23466d581aabf7
Vinicius Silva <vsaguiar@outlook.com>
2023-06-06 17:03:25 -0300

    Implementando view model LancheListViewModel

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -15,8 +16,14 @@ namespace LanchesMac.Controllers
         public IActionResult List()
         {
             // Retornando uma lista de Lanches
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches; // Retornando uma lista de Lanches
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
         }
     }
 }
