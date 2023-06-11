e3be9709861aae04537a65d595c77b20308d1503
Vinicius Silva <vsaguiar@outlook.com>
2023-06-11 18:56:54 -0300

    Criando rota para exibir lanches por categoria

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -48,6 +48,13 @@ public class Program
 
         app.UseAuthorization();
 
+
+        app.MapControllerRoute(
+            name: "categoriaFiltro", 
+            pattern: "Lanche/{action}/{categoria?}", 
+            defaults: new { Controller = "Lanche", action = "List" });
+
+
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
