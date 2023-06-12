8e80d05c3c7908198e93044a225218ad4e1d59ee
Vinicius Silva <vsaguiar@outlook.com>
2023-06-11 21:45:14 -0300

    Incluindo link e button na exibição dos lanches

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -50,5 +50,12 @@ namespace LanchesMac.Controllers
             };
             return View(lanchesListViewModel);
         }
+
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
+        }
+
     }
 }
