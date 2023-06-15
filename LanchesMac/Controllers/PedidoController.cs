258b7828fa157cbe06f25fa6d10e07fe1f250ec7
Vinicius Silva <vsaguiar@outlook.com>
2023-06-15 14:56:55 -0300

    Criando PedidoController

---
+++ LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,33 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+
+        // Formulário de confirmação para o cliente
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+
+        // Trata o formulário de confirmação
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
