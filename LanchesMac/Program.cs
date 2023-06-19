db6afa5afcbf0d3bb0ec0b87527617475888b8b0
Vinicius Silva <vsaguiar@outlook.com>
2023-06-19 18:22:49 -0300

    Criando view register

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -23,6 +23,17 @@ public class Program
         // Serviço do Identity
         builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+            // Default Password settings
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 3;
+            options.Password.RequiredUniqueChars = 1;
+        });
+
         builder.Services.AddTransient<ILancheRepository, LancheRepository>();
         builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
