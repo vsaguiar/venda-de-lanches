556562af19073f57ca9156b42379120d3ad7e718
Vinicius Silva <vsaguiar@outlook.com>
2023-06-20 16:22:43 -0300

    Implementando o logout

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -84,5 +84,15 @@ namespace LanchesMac.Controllers
             }
             return View(registroVM);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
