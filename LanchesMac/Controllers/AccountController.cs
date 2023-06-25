b2edca811ca2de28e4223586d31791b080416a4a
Vinicius Silva <vsaguiar@outlook.com>
2023-06-25 19:08:46 -0300

    View accessdenied e menu para usuário admin

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -95,5 +95,10 @@ namespace LanchesMac.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
