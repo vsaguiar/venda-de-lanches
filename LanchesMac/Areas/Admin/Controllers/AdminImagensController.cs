a94266a9e49f3bc5fa8bb3504d0e953f0775d98f
Vinicius Silva <vsaguiar@outlook.com>
2023-07-30 20:01:51 -0300

    Enviando arquivos

--- LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -23,5 +23,55 @@ namespace LanchesMac.Areas.Admin.Controllers
         {
             return View();
         }
+
+
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Error: Quantidade de arquivos excedeu o limite";
+                return View(ViewData);
+            }
+
+            long size = files.Sum(f => f.Length);
+
+            var filePathsName = new List<string>(); // armezando os nomes dos arquivos que foram enviados
+
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos); // caminho completo do local onde os arquivos serão armazenados
+
+            foreach (var formFile in files)
+            {
+                if (formFile.FileName.Contains(".jpg") || formFile.FileName.Contains(".gif") || formFile.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
+
+                    filePathsName.Add(fileNameWithPath);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            if (files.Count > 1)
+            {
+                ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor com tamanho de: {size} bytes";
+            }
+            else
+            {
+                ViewData["Resultado"] = $"{files.Count} arquivo foi enviado ao servidor com tamanho de: {size} bytes";
+            }
+
+            ViewBag.Arquivos = filePathsName;
+
+            return View(ViewData);
+        }
     }
 }
