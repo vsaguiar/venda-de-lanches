3b79f6c27be7ef23340da57d7434dfae96faccec
Vinicius Silva <vsaguiar@outlook.com>
2023-07-30 03:04:18 -0300

    Exibindo as opções para upload e consulta de imagens

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig; // através dessa classe podemos obter o nome da pasta onde os arquivos serão salvos
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public AdminImagensController(IWebHostEnvironment hostingEnvironment, IOptions<ConfigurationImagens> myConfiguration)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _myConfig = myConfiguration.Value;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
